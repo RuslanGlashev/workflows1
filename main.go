8c120846622e493522beea59f1ab1114e9443a99
RuslanGlashev <ruslanglashev797@mail.ru>
2025-04-06 18:59:33 +0300

    commits

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+  GNU nano 8.2                        main.go
+package main
+
+func main() {
+
+}
+
+func MaxInt(a, b int) int {
+    if a >= b {
+        return a
+    }
+
+    return b
+}
+
 package main
 
 func main() {
